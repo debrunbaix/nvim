d2943ca3188b4a6cf01dd8c9a1afd4394f353495
sasha.deroubaix <sasha.deroubaix@cyna-it.fr>
2025-03-25 16:39:41 +0100

    feat(cmp.lua): Better CMP with lsp integration.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,12 +16,13 @@ return {
     {
 	"neovim/nvim-lspconfig",
 	config = function()
+	    local capabilities = require('cmp_nvim_lsp').default_capabilities()
 	    local lspconfig = require("lspconfig")
 
-	    lspconfig.lua_ls.setup({})
-	    lspconfig.clangd.setup({})
-	    lspconfig.pyright.setup({})
-	    lspconfig.rust_analyzer.setup({})
+	    lspconfig.lua_ls.setup({ capabilities = capabilities })
+	    lspconfig.clangd.setup({ capabilities = capabilities })
+	    lspconfig.pyright.setup({ capabilities = capabilities })
+	    lspconfig.rust_analyzer.setup({ capabilities = capabilities })
 
 	    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 	end
