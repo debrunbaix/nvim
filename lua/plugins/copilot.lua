197f9fdbcb1c90c2647eb49f45f3e876da316f0f
Debrunbaix <sasha.deroubaix.dev@proton.me>
2025-06-23 20:28:10 +0200

    added todo and deactivate copilot as default

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,12 @@
 return {
     "github/copilot.vim",
+    config = function()
+        vim.api.nvim_create_autocmd("BufEnter", {
+            pattern = "*",
+            callback = function()
+                vim.cmd("Copilot disable")
+            end,
+        })
+    end,
 }
+
